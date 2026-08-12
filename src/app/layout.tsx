864b55eacff5467624ee50b791c758804843bff1
bilregistret <salman.izhar@bilregistret.ai>
2026-08-13 00:12:59 +0600

    feat: Add Google Tag Manager integration and update logo in Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata, Viewport } from "next";
 import { Barlow_Condensed } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import { services, site } from "@/lib/site";
+
+const gtmId = "GTM-5RRZNKGP";
 
 const barlow = Barlow_Condensed({
   subsets: ["latin"],
@@ -99,7 +102,22 @@ export default function RootLayout({
   };
   return (
     <html lang="sv" className={barlow.variable}>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`}
+      </Script>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <a className="skip-link" href="#main">
           Hoppa till innehåll
         </a>
